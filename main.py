8b65969bf56497e346d9d8a3e6cce7e709b2b133
Student User <student@rcsd.ca>
2017-04-11 10:41:03 -0600

    Starting main.py, and breaking things!!!!! Try to fix it!

---
+++ main.py
@@ -0,0 +1,26 @@
+# Import those moduels!!!!
+import sys
+import pygame
+
+#Settin' up the window!
+pygame.init()
+screen = pygame.display.set_mode((700, 450))
+done = False
+pygame.display.set_caption("Spitfire Pre-Alpha")
+pygame.display.flip()
+
+
+#Exit Control
+def(qui)
+        pygame.quit()
+        quit()
+
+if event.type == pygame.QUIT:
+	qui()
+
+
+def(mainloop)
+    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(100, 303, 60, 60))
+    pygame.display.flip()
+
+mainloop()
