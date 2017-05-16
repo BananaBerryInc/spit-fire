20f8039bb8d71fe74b397625d3f778aeb34199ca
johnnyw3 <johnathan8080@gmail.com>
2017-05-16 11:17:22 -0600

    Updated run_main.py.bat

--- run_main.py.bat
+++ run_main.py.bat
@@ -1,3 +1,16 @@
+@echo off
+
+::If you have Python installed, this should work......
+python main.py 
+
+:: This is where Johnnyw3 puts his files, so don't mind this 
 D:\Grade8\Utilities\Python\python.exe main.py
+:: Drive might be mounted as e:\ for some reason...
 E:\Grade8\Utilities\Python\python.exe main.py
+
+set /p id="if you just got an error, type in the path to python.exe here"
+
+%id% main.py
+
+::Sleep to see if there errors in the code...
 pause
